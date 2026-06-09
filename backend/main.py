6d16113d3aca1debf65acf4a5229ca1a8b029b53
Hussain Arif <hussainarifkl@gmail.com>
2026-06-09 11:50:07 +0200

    Support for Cloudflarae

--- backend/main.py
+++ backend/main.py
@@ -3,10 +3,14 @@ from pathlib import Path
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
-
 from routes.trading_one_month import router as trading_one_month_router
 from routes.bank_one_month import router as bank_one_month_router
 
+print("hi")
+try:
+    from workers import WorkerEntrypoint
+except:
+    pass
 BASE_DIR = Path(__file__).resolve().parent
 INDEX_FILE = BASE_DIR / "index.html"
 
@@ -29,3 +33,15 @@ async def root():
 
 app.include_router(trading_one_month_router)
 app.include_router(bank_one_month_router)
+
+
+try:
+
+    class Default(WorkerEntrypoint):
+        async def fetch(self, request):
+            import asgi
+
+            return await asgi.fetch(app, request.js_object, self.env)
+
+except:
+    pass
