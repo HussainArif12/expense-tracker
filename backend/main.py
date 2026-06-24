a016658a8f5f5263d4293b788b48df98a16f3408
Hussain Arif <hussainarifkl@gmail.com>
2026-06-24 18:03:32 +0200

    Add self host option

--- backend/main.py
+++ backend/main.py
@@ -8,20 +8,30 @@ from routes.bank_one_month import router as bank_one_month_router
 from routes.verdict import router as verdict_router
 from routes.bank_multi_month import router as bank_multi_month_router
 from routes.trading_multi_month import router as trading_multi_month_router
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent
 INDEX_FILE = BASE_DIR / "index.html"
+SELF_HOST = os.environ.get("SELF_HOST")
 
 app = FastAPI()
+origins = (
+    [
+        "http://localhost:3000",
+        "https://tanstack-start-app.hussainarifkl.workers.dev",
+        "https://expense-tracker.hussainarifkl.workers.dev",
+    ]
+    if SELF_HOST
+    else ["*"]
+)
 
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://tanstack-start-app.hussainarifkl.workers.dev",
-        "https://expense-tracker.hussainarifkl.workers.dev",
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
